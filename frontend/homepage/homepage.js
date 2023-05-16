fdcd81cc72d1eb14198c834b51de3ab808a2e2d4
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2023-05-17 01:02:33 +0330

    add homepage - complete navigation bar

---
+++ frontend/homepage/homepage.js
@@ -0,0 +1,6 @@
+function setNavLinksHeight(){
+    let NL = document.getElementsByClassName("navLink");
+    for(let i=0; i<NL.length ; i++){
+        NL[i].style.height = NL[i].clientHeight / 2 + "px";
+    }
+}
