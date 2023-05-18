5831f1556a383ab4e32750f19de03cd21e852007
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2023-05-18 22:37:46 +0330

    append new reviews

--- frontend/homepage/homepage.js
+++ frontend/homepage/homepage.js
@@ -2,4 +2,16 @@ document.addEventListener('DOMContentLoaded', function() {
     let navBarSelector = document.getElementsByClassName('homepageNavbar')[0].getElementsByTagName('ul')[0].
                             getElementsByTagName('li')[0].getElementsByTagName('a')[0].getElementsByTagName('span')[0];
     navBarSelector.style.color = '#ffffff';
+
+    showReviewHandler();
 });
+
+function onloadHandler(){
+    showReviewHandler();
+}
+
+function showReviewHandler(){
+    let review = document.getElementsByClassName('tm-timeline-item')[0].cloneNode(true);
+
+    document.getElementsByClassName('tm-section-mb')[0].getElementsByClassName('col-lg-12')[0].appendChild(review);
+}
