0bdf9e39b2759baa763360cdac97fe33c88c9330
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2023-05-18 22:07:50 +0330

    update navbar in homepage

---
+++ frontend/homepage/homepage.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let navBarSelector = document.getElementsByClassName('homepageNavbar')[0].getElementsByTagName('ul')[0].
+                            getElementsByTagName('li')[0].getElementsByTagName('a')[0].getElementsByTagName('span')[0];
+    navBarSelector.style.color = '#ffffff';
+});
