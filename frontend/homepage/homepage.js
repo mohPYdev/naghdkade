76cb365aca6990dce55fe9f30f611a6b0a0114a8
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2023-05-21 08:23:59 +0330

    Update homepage.js

--- frontend/homepage/homepage.js
+++ frontend/homepage/homepage.js
@@ -1,14 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let navBarSelector = document.getElementsByClassName('homepageNavbar')[0].getElementsByTagName('ul')[0].
-                            getElementsByTagName('li')[0].getElementsByTagName('a')[0].getElementsByTagName('span')[0];
-    navBarSelector.style.color = '#ffffff';
-
-});
-
-function onloadHandler(){
+function homepageOnloadHandler(){
     showReviewHandler();
 }
 
+//show reviews
 function showReviewHandler(){
     let review = document.getElementsByClassName('tm-timeline-item')[0].cloneNode(true);
 
