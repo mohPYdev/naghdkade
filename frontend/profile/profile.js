9c7c3846e5718f2b3965dea25b547894af2ee422
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2023-06-08 18:58:27 +0330

    Create profile.js

---
+++ frontend/profile/profile.js
@@ -0,0 +1,10 @@
+function personReviewsOnloadHandler(){
+    showMovieHandler();
+}
+
+//show reviews
+function showMovieHandler(){
+    let movie = document.getElementsByClassName('tm-timeline-item')[0].cloneNode(true);
+
+    document.getElementsByClassName('tm-section-mb')[0].getElementsByClassName('col-lg-12')[0].appendChild(movie);
+}
