4c70f4abf2a8038d5f3acee56ae98a64540f5c25
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2023-06-09 16:05:21 +0330

    mov-ser details: show related reviews

--- frontend/mov-ser details/mov-serDetails.js
+++ frontend/mov-ser details/mov-serDetails.js
@@ -1,3 +1,9 @@
 function movserDetailsOnloadHandler(){
-    
+    showReviewsHandler();
+}
+
+function showReviewsHandler(){
+    let movie = document.getElementsByClassName('tm-timeline-item')[1].cloneNode(true);
+
+    document.getElementsByClassName('tm-section-mb')[1].getElementsByClassName('col-lg-12')[0].appendChild(movie);
 }
