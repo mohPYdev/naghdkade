56f97cf78dbf5977e47197d37e0ac6732b2a65c1
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2023-06-09 14:39:38 +0330

    add showFollowArea function

--- frontend/profile/profile.js
+++ frontend/profile/profile.js
@@ -1,10 +1,18 @@
 function personReviewsOnloadHandler(){
-    showMovieHandler();
+    showPersonReviewsHandler();
+    showFollowArea();
 }
 
 //show reviews
-function showMovieHandler(){
+function showPersonReviewsHandler(){
     let movie = document.getElementsByClassName('tm-timeline-item')[0].cloneNode(true);
 
     document.getElementsByClassName('tm-section-mb')[0].getElementsByClassName('col-lg-12')[0].appendChild(movie);
+}
+
+function showFollowArea(){
+    /*if(){
+    document.getElementsByClassName('following-follower')[0].style.display = "none";
+
+    }*/
 }
