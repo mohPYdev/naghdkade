9199d36cf094282a46e27641372fa4217a840ced
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2023-06-09 21:25:56 +0330

    profile ready for interaction with backend

--- frontend/profile/profile.js
+++ frontend/profile/profile.js
@@ -10,6 +10,8 @@ function showPersonReviewsHandler(){
     let movie = document.getElementsByClassName('tm-timeline-item')[0].cloneNode(true);
 
     document.getElementsByClassName('tm-section-mb')[0].getElementsByClassName('col-lg-12')[0].appendChild(movie);
+
+    //show reviews
 }
 
 function showFollowArea(){
