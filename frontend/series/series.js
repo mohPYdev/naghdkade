4913b6f5eba9f6af914232c50d6d65976d8fa84f
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2023-06-09 21:19:19 +0330

    series ready for interaction with backend

--- frontend/series/series.js
+++ frontend/series/series.js
@@ -7,4 +7,6 @@ function showSerieHandler(){
     let serie = document.getElementsByClassName('tm-timeline-item')[0].cloneNode(true);
 
     document.getElementsByClassName('tm-section-mb')[0].getElementsByClassName('col-lg-12')[0].appendChild(serie);
+    
+    //show series
 }
