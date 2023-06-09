94f9147a2df73028f46667d93ce08758fe3e54ab
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2023-06-09 21:11:40 +0330

    movies ready for interaction with backend

--- frontend/movies/movies.js
+++ frontend/movies/movies.js
@@ -7,4 +7,6 @@ function showMovieHandler(){
     let movie = document.getElementsByClassName('tm-timeline-item')[0].cloneNode(true);
 
     document.getElementsByClassName('tm-section-mb')[0].getElementsByClassName('col-lg-12')[0].appendChild(movie);
+
+    //show movies
 }
