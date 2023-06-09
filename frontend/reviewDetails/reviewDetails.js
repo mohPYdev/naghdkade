8824f21fe27c2d6ef38419ac526e69d345a3a760
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2023-06-09 21:04:45 +0330

    reviewDetails ready for interaction with backend

--- frontend/reviewDetails/reviewDetails.js
+++ frontend/reviewDetails/reviewDetails.js
@@ -1,4 +1,5 @@
 function reviewDetailsOnloadHandler(){
+    //fetch review details
     showCommentsHandler();
 }
 
@@ -6,4 +7,10 @@ function showCommentsHandler(){
     let movie = document.getElementsByClassName('tm-timeline-item')[1].cloneNode(true);
 
     document.getElementsByClassName('tm-section-mb')[1].getElementsByClassName('col-lg-12')[0].appendChild(movie);
+
+    //show comments
+}
+
+function submitComment(){
+    //add new comment
 }
