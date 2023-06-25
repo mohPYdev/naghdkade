3fba0fdfa9627006049b4a690119ffdeb70e026a
Khandanmelika <122014155+Khandanmelika@users.noreply.github.com>
2023-06-25 12:27:05 +0330

    link followings & followers to api

--- frontend/server/profile/follow.js
+++ frontend/server/profile/follow.js
@@ -15,16 +15,29 @@ function followingOnloadHandler(){
     if(token == null || token == undefined || token == "undefined"){
       top.location = '../signin-signup.html';
     }
-    fetch(`http://localhost:8000/api/social/follow/`, {
+
+    
+        document.getElementsByClassName('tm-timeline-item')[0].style.display = 'none'
+        
+        fetch(`http://localhost:8000/api/social/follow/`, {
         headers: {
             'Authorization': `Token ${token}`
         }
         })
         .then(response => response.json())
         .then(data => {
-            console.log(data.followings);
-            
-            })
+            let followingArea = document.getElementsByClassName('tm-timeline-item')[0].cloneNode(true);
+            followingArea.style.display = 'block';
+
+            let followings = data.followings;
+            for(let i=0 ; i<followings.length ; i++){
+                followingArea.getElementsByClassName('tm-timeline-item-inner')[0].getElementsByClassName('rounded-circle')[0].src = followings[i].image;
+                followingArea.getElementsByClassName('tm-timeline-item-inner')[0].getElementsByClassName('tm-timeline-description-wrap')[0].getElementsByClassName('tm-bg-dark tm-timeline-description')[0].getElementsByTagName('a')[0].getElementsByTagName('h3')[0].textContent = followings[i].username;
+                followingArea.getElementsByClassName('tm-timeline-item-inner')[0].getElementsByClassName('tm-timeline-description-wrap')[0].getElementsByClassName('tm-bg-dark tm-timeline-description')[0].getElementsByTagName('a')[0].href = `../profile/otherProfile.html?id=${followings[i].id}`;
+                
+            }
+
+        })
         .catch(error => {
             console.error('Error:', error);
         });
@@ -34,17 +47,29 @@ function followerOnloadHandler(){
     token = getCookieValue('token');
     if(token == null || token == undefined || token == "undefined"){
       top.location = '../signin-signup.html';
-    }q
-    fetch(`http://localhost:8000/api/social/follow/`, {
+    }
+    
+        document.getElementsByClassName('tm-timeline-item')[0].style.display = 'none'
+        
+        fetch(`http://localhost:8000/api/social/follow/`, {
         headers: {
             'Authorization': `Token ${token}`
         }
         })
         .then(response => response.json())
         .then(data => {
-            console.log(data.followers);
-            
-            })
+            let followerArea = document.getElementsByClassName('tm-timeline-item')[0].cloneNode(true);
+            followerArea.style.display = 'block';
+
+            let followers = data.followers;
+            for(let i=0 ; i<followings.length ; i++){
+                followingArea.getElementsByClassName('tm-timeline-item-inner')[0].getElementsByClassName('rounded-circle')[0].src = followers[i].image;
+                followingArea.getElementsByClassName('tm-timeline-item-inner')[0].getElementsByClassName('tm-timeline-description-wrap')[0].getElementsByClassName('tm-bg-dark tm-timeline-description')[0].getElementsByTagName('a')[0].getElementsByTagName('h3')[0].textContent = followers[i].username;
+                followingArea.getElementsByClassName('tm-timeline-item-inner')[0].getElementsByClassName('tm-timeline-description-wrap')[0].getElementsByClassName('tm-bg-dark tm-timeline-description')[0].getElementsByTagName('a')[0].href = `../profile/otherProfile.html?id=${followers[i].id}`;
+                
+            }
+
+        })
         .catch(error => {
             console.error('Error:', error);
         });
